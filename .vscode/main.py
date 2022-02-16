948d05e40a9f764dea0007df34a0ebcf0d204e4a
jowallace1 <john.nagib.wallace@gmail.com>
2022-02-16 16:22:48 -0500

    Added interrupt

--- .vscode/main.py
+++ .vscode/main.py
@@ -2,28 +2,41 @@ from machine import Pin
 from machine import UART
 import numpy as np
 
-CRESP = Pin(32, Pin.IN)
+CRESP = Pin(32, Pin.IN)  # CRESP pin (FOR INTERRUPT)
+BE = Pin(27, Pin.IN)  # BE pin (CAN BE READ THROUGH LSTATUS REGESTER IF NEEDED)
 CMD = Pin(31, Pin.OUT)  # CMD pin
 CTS = Pin(29, Pin.IN)  # CTS pin
-txPin = Pin(21, Pin.OUT)  # tx pin
-rxPin = Pin(22, Pin.IN)  # rx pin
-MODE_IND = Pin(24, Pin.IN)
+txPin = Pin(21, Pin.OUT)  # TX pin
+rxPin = Pin(22, Pin.IN)  # RX pin
+MODE_IND = Pin(24, Pin.IN)  # MODE_IND pin
 uart = UART(1, 9600, tx=txPin, rx=rxPin)  # initialize UART
 
-# loop:
-# while transmitArray(data) == False:
-
+# used to configure the HumPRO's settings
 def configure():
     if MODE_IND.value == 0:
         CMD.value(0)
 
 
-def transmitArray(data):
+# used to transmit data to the HumPRO for RF transmission
+def transmitData(data):
     if CTS.value == 0:
         CMD.value(1)
         uart.write(data)
         CMD.value(0)
 
-        return False
-    else:
-        return True
+    # hold until HumPRO buffer is empty, indicating all data has been transmitted
+    while True:
+        if BE.value == 1:
+            # ADD EEXFLAG REGISTER SAMPLING HERE --> RETURN TRUE IF NO ERRORS, FALSE IF ERRORS
+            return
+
+
+# used to read data from the uart connection with the HumPRO
+def readData():
+    return
+
+
+# attach interrupt to CRESP so that data is read when it goes high
+CRESP.irq(trigger=Pin.IRQ_RISING, handler=readData)
+
+transmitData(5)
