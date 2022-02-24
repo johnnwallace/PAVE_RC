d87db48bf013fdaf9d8da72d45221f3348438f43
William Huang <williamhuang314@gmail.com>
2022-02-24 00:06:57 -0500

    Co-authored-by: jowallace1 <jowallace1@users.noreply.github.com>

--- .vscode/remoteControl.py
+++ .vscode/remoteControl.py
@@ -1,19 +1,20 @@
 # from machine import Pin, UART
+import machine
 import numpy as np
 
-# controlString is a 4-element np array with 0/1 corresponding to unpressed/pressed for each of 4 arrow buttons
-# First element = forward. Second element = backward. Third element = left. Fourth element = right
+# controlString is a 8-element np array with 0/1 corresponding to unpressed/pressed for the 4 pad buttons and 4 circle buttons
+# SOFT forward, backward, left, right
+# HARD forward, backward, left, right
 # Returns two element array (s,t) that is sent to motor to tell it how many degrees to turn and how much throttle to use
 def readRemote(controlString):
-    
+
     controlArray = np.array(
-        [[0, 0, -10, 10], [20, -20, 0, 0]]
+        [[0, 0, -10, 10, 0, 0, -30, 30], [10, -10, 0, 0, 30, -30, 0, 0]]
     )  # determines contributions to s, t based on each button
 
     command = np.matmul(controlArray, controlString)  # (s, t)
 
     return command
-
 
 # autoCommand are values from autonomous algorithm assuming other people will decide whether this is coming from GPS or CV
 # remoteInput it a 4 element 0/1 column vector corresponding to forward, backward, left, right buttons
@@ -26,3 +27,4 @@ def control(autoCommand, remoteInput, isAuto):
         return autoCommand
 
 def autonomousSteering(currentAngle, desiredAngle, thetaDot, thetaIntegral):
+    return
