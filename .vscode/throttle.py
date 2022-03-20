f1a8dc83f90c669bd123f709fc922147501be040
jowallace1 <john.nagib.wallace@gmail.com>
2022-03-20 17:05:58 -0400

    Update throttle.py

--- .vscode/throttle.py
+++ .vscode/throttle.py
@@ -3,3 +3,4 @@ import numpy as np
 forecastTime = 2  # look this many seconds in the future
 maxLateral = 20  # assuming maximum lateral acceleration of 20 m/s/s
 maxLongitudinal = 30  # assuming maximum longitudinal acceleration of 20 m/s/s
+
