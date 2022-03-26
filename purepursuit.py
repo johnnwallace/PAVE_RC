a829fafd6017d7809d7f2d487d9f685a84f09ab4
J-Etiosa-Omeike <43898679+J-Etiosa-Omeike@users.noreply.github.com>
2022-03-26 12:56:41 -0400

    Create purepursuit.py

---
+++ purepursuit.py
@@ -0,0 +1,287 @@
+
+"""
+Path tracking simulation with pure pursuit steering and PID speed control.
+author: Atsushi Sakai (@Atsushi_twi)
+        Guillaume Jacquenot (@Gjacquenot)
+"""
+
+## to get a virtual environment, run "python3 -m venv venv" and then ". venv/bin/activate"
+# log of GPS files, read off the lines of this log to find current location, 
+
+# TASKS:
+# reach out to Ken or Zixu for reach-avoid code and ILQR games code
+# implement pure pursuit assuming no obstacles that need to be avoided are in line of vision
+# generate log file (csv?) of GPS coordinates (current)
+# generate log file of ideal path (waypoints) 
+# 1.) Read in target GPS coordinates
+# 2.) Adjust distanceTo method for current -> waypoint GPS coordinates for control 
+# Go to Google Maps and get sample loop from race track (log file of waypoints)
+
+# Researching how to get yaw
+# Add PID Controller
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import csv
+
+# Parameters
+k = 0.1  # look forward gain
+Lfc = 2.0  # [m] look-ahead distance
+Kp = 1.0  # speed proportional gain
+dt = 0.1  # [s] time tick
+WB = 2.9  # [m] wheel base of vehicle
+
+show_animation = True
+
+class PIDController:
+    def __init__(self, setPoint, Kp, Ki, Kd):
+        self.setPoint = setPoint
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.integralError = self.lastError = 0
+
+    def updateError(self, currentState, dt):  # update error and setpoint values
+        self.error = self.setPoint - currentState  # get error
+        self.integralError += self.error * dt  # get cumulative error
+        self.derivativeError = (
+            self.error - self.lastError
+        ) / dt  # get derivative of error
+        self.lastError = self.error  # save current error
+
+    # maybe create a ramp to avoid integral windup
+    def updateSetpoint(self, newSetPoint):
+        self.setPoint = newSetPoint
+
+    def evaluate(self):  # return command value
+        return (
+            self.Kp * self.error
+            + self.Ki * self.integralError
+            + self.Kd * self.derivativeError
+        )
+
+class State:
+
+    def __init__(self, x=0.0, y=0.0, yaw=0.0, v=0.0):
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+        self.v = v
+        self.rear_x = self.x - ((WB / 2) * math.cos(self.yaw))
+        self.rear_y = self.y - ((WB / 2) * math.sin(self.yaw))
+
+    def update(self, a, delta):
+        self.x += self.v * math.cos(self.yaw) * dt
+        self.y += self.v * math.sin(self.yaw) * dt
+        self.yaw += self.v / WB * math.tan(delta) * dt
+        self.v += a * dt
+        self.rear_x = self.x - ((WB / 2) * math.cos(self.yaw))
+        self.rear_y = self.y - ((WB / 2) * math.sin(self.yaw))
+
+    def calc_distance(self, point_x, point_y):
+        dx = self.rear_x - point_x
+        dy = self.rear_y - point_y
+        return math.hypot(dx, dy)
+
+    # get from reha's github repo 
+    def get_x():
+        return 1
+
+    def get_y():
+        return -1
+    
+    def get_yaw():
+        return 2 
+
+
+class States:
+
+    def __init__(self):
+        self.x = []
+        self.y = []
+        self.yaw = []
+        self.v = []
+        self.t = []
+
+    def append(self, t, state):
+        self.x.append(state.x)
+        self.y.append(state.y)
+        self.yaw.append(state.yaw)
+        self.v.append(state.v)
+        self.t.append(t)
+
+
+def proportional_control(target, current):
+    a = Kp * (target - current)
+
+    return a
+
+
+class TargetCourse:
+
+    def __init__(self, cx, cy):
+        self.cx = cx
+        self.cy = cy
+        self.old_nearest_point_index = None
+
+    def search_target_index(self, state):
+
+        # To speed up nearest point search, doing it at only first time.
+        if self.old_nearest_point_index is None:
+            # search nearest point index
+            dx = [state.rear_x - icx for icx in self.cx]
+            dy = [state.rear_y - icy for icy in self.cy]
+            d = np.hypot(dx, dy)
+            ind = np.argmin(d)
+            self.old_nearest_point_index = ind
+        else:
+            ind = self.old_nearest_point_index
+            distance_this_index = state.calc_distance(self.cx[ind],
+                                                      self.cy[ind])
+            while True:
+                distance_next_index = state.calc_distance(self.cx[ind + 1],
+                                                          self.cy[ind + 1])
+                if distance_this_index < distance_next_index:
+                    break
+                ind = ind + 1 if (ind + 1) < len(self.cx) else ind
+                distance_this_index = distance_next_index
+            self.old_nearest_point_index = ind
+
+        Lf = k * state.v + Lfc  # update look ahead distance
+
+        # search look ahead target point index
+        while Lf > state.calc_distance(self.cx[ind], self.cy[ind]):
+            if (ind + 1) >= len(self.cx):
+                break  # not exceed goal
+            ind += 1
+
+        return ind, Lf
+
+# change this to the ST method for boat steering 
+
+# input error and current direction (state vector)
+def pure_pursuit_steer_control(state, trajectory, pind):
+    ind, Lf = trajectory.search_target_index(state)
+
+    if pind >= ind:
+        ind = pind
+
+    if ind < len(trajectory.cx):
+        tx = trajectory.cx[ind]
+        ty = trajectory.cy[ind]
+    else:  # toward goal
+        tx = trajectory.cx[-1]
+        ty = trajectory.cy[-1]
+        ind = len(trajectory.cx) - 1
+
+    alpha = math.atan2(ty - state.rear_y, tx - state.rear_x) - state.yaw
+
+    delta = math.atan2(2.0 * WB * math.sin(alpha) / Lf, 1.0)
+
+    return delta, ind
+
+
+def plot_arrow(x, y, yaw, length=1.0, width=0.5, fc="r", ec="k"):
+    """
+    Plot arrow
+    """
+
+    if not isinstance(x, float):
+        for ix, iy, iyaw in zip(x, y, yaw):
+            plot_arrow(ix, iy, iyaw)
+    else:
+        plt.arrow(x, y, length * math.cos(yaw), length * math.sin(yaw),
+                  fc=fc, ec=ec, head_width=width, head_length=width)
+        plt.plot(x, y)
+
+
+def main():
+    #  target course
+    with open("map_flask/data.csv", mode="r", encoding="utf-8-sig") as csv_file: #had to use relative path
+        csv_reader = csv.reader(csv_file)
+        cx = []
+        cy = []
+        for row in csv_reader: 
+            cx.append(float(row[1]))
+            cy.append(float(row[2]))
+
+    # write control method to write target_speed as a function of state, controls, etc
+    target_speed = 10.0 / 3.6  # [m/s]
+
+    T = 100.0  # max simulation time
+
+    # initial state
+    x = get_x()
+    y = get_y()
+    yaw =  get_yaw()
+    state = State(x=-0.0, y=-3.0, yaw=0.0, v=0.0)
+
+    lastIndex = len(cx) - 1
+    time = 0.0
+    states = States()
+    states.append(time, state)
+    target_course = TargetCourse(cx, cy)
+    target_ind, _ = target_course.search_target_index(state)
+
+    while T >= time and lastIndex > target_ind:
+
+        # Calc control input
+        ai = proportional_control(target_speed, state.v)
+        di, target_ind = pure_pursuit_steer_control(
+            state, target_course, target_ind)
+
+        state.update(ai, di)  # Control vehicle
+
+        time += dt
+        states.append(time, state)
+
+        if show_animation:  # pragma: no cover
+            plt.cla()
+            # for stopping simulation with the esc key.
+            plt.gcf().canvas.mpl_connect(
+                'key_release_event',
+                lambda event: [exit(0) if event.key == 'escape' else None])
+            plot_arrow(state.x, state.y, state.yaw)
+            plt.plot(cx, cy, "-r", label="course")
+            plt.plot(states.x, states.y, "-b", label="trajectory")
+            plt.plot(cx[target_ind], cy[target_ind], "xg", label="target")
+            plt.axis("equal")
+            plt.grid(True)
+            plt.title("Speed[km/h]:" + str(state.v * 3.6)[:4])
+            plt.pause(0.001)
+
+    # Test
+    assert lastIndex >= target_ind, "Cannot goal"
+
+    if show_animation:  # pragma: no cover
+        plt.cla()
+        plt.plot(cx, cy, ".r", label="course")
+        plt.plot(states.x, states.y, "-b", label="trajectory")
+        plt.legend()
+        plt.xlabel("x[m]")
+        plt.ylabel("y[m]")
+        plt.axis("equal")
+        plt.grid(True)
+
+        plt.subplots(1)
+        plt.plot(states.t, [iv * 3.6 for iv in states.v], "-r")
+        plt.xlabel("Time[s]")
+        plt.ylabel("Speed[km/h]")
+        plt.grid(True)
+        plt.show()
+
+
+if __name__ == '__main__':
+    print("Pure pursuit path tracking simulation start")
+    main()
+
+# Pins 8 and 9 are latitude and longitude
+# Step 1: Figure out how to get GPS data into python pure pursuit
+    # a.) Predetermined coordinates -- and if we do this redevelop control algothihm so that it avoids other vehicles, land, etc.
+    # b.) we train our robot to figure out how to determine proper coordinates itself 
+# Step 2: CONTROLS:
+    # figure out how we're going to implement the control system (i.e., turn coordinates into control actions for the boat)
+    # reach out to hardware people about that
+
+# What we'll do:
+    # Reconvene in 1-2 weeks and figure out how to take GPS coordinates, implement pure pursuit using them and then map them on Folium to display trajectory 
