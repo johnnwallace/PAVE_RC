b15fd7d7ec8678b255f88a1d23407a4db5ac1310
jowallace1 <john.nagib.wallace@gmail.com>
2022-03-26 12:10:12 -0400

    Finished circle generation

--- .vscode/throttle.py
+++ .vscode/throttle.py
@@ -1,38 +1,77 @@
+import math
 import numpy as np
 from steering import PIDController as PID
+
+# from steering import PIDController as PID
 
 maxLateral = 20  # assuming maximum lateral acceleration of 20 m/s/s
 maxLongitudinal = 30  # assuming maximum longitudinal acceleration of 20 m/s/s
 
 
+class Circle:
+    def perpBis(point1, point2):
+        m1 = -1 / ((point2[1] - point1[1]) / (point2[0] - point1[0]))
+        mp1 = (points[:, 0] + points[:, 1]) / 2
+        b1 = mp1[1] - m1 * mp1[0]
+
+        return m1, b1
+
+    def getCircle(points):
+        # find 2 perpendicular bisectors
+        # check if y1 == y0 or y2 == y0 or x1 == x0 or x2 == x0
+        if (
+            np.array_equal(points[:, 0], points[:, 1])
+            or np.array_equal(points[:, 0], points[:, 2])
+            or np.array_equal(points[:, 1], points[:, 2])
+        ):
+            raise ValueError("Two or more points are the same")
+
+        # find intersection of 2 perpendicular bisectors, considering cases when slope = 0 or is undefined
+        if points[1, 0] == points[1, 1]:
+            xIntersect = (points[0, 0] + points[0, 1]) / 2
+        elif points[1, 0] == points[1, 2]:
+            xIntersect = (points[0, 0] + points[0, 2]) / 2
+        else:
+            m1, b1 = Circle.perpBis(points[:, 0], points[:, 1])
+            m2, b2 = Circle.perpBis(points[:, 0], points[:, 2])
+
+            xIntersect = (b2 - b1) / (m1 - m2)
+
+        if points[1, 0] == points[1, 1]:
+            yIntersect = (points[1, 0] + points[1, 1]) / 2
+        elif points[1, 0] == points[1, 2]:
+            yIntersect = (points[1, 0] + points[1, 2]) / 2
+        else:
+            m1, b1 = Circle.perpBis(points[:, 0], points[:, 1])
+            m2, b2 = Circle.perpBis(points[:, 0], points[:, 2])
+
+            yIntersect = (b2 - b1) / (m1 - m2)
+
+        radius = math.sqrt(
+            (points[0, 2] - xIntersect) ** 2 + (points[1, 2] - yIntersect) ** 2
+        )
+
+        return (xIntersect, yIntersect, radius)
+
+    def getCentripetal(points, velocity):
+        radius = Circle.getCircle(points)[2]
+        return velocity * velocity / radius
+
+
 class Throttle:
-    def __init__(self, maxLat, maxLon, setPoint, Kp, Ki, Kd):
+    def __init__(self, maxLat, maxLon, points, lookAhead, Kp, Ki, Kd):
         self.maxLat = maxLat
         self.maxLon = maxLon
+        self.points = points
+        self.lookAheadDist = lookAhead
+        self.current = 0
 
-        pid = PID(setPoint, Kp, Ki, Kd)
+        self.controller = PID(0, Kp, Ki, Kd)
 
-    def getCircle(self, points):
-        # find 2 perpendicular bisectors
-        m1 = -1 / ((points[1, 1] - points[1, 0]) / (points[0, 1] - points[0, 0]))
-        mp1 = np.mean(points[:, 0], points[:, 1])
-        b1 = mp1[1, 0] - m1 * mp1[0, 0]
+    def lookAhead(self):
+        return Circle.getCentripetal()
+    
+points = np.array([(-5, 0, 5), (0, -5, 0)])
+velocity = 10
 
-        m2 = -1 / ((points[1, 2] - points[1, 0]) / (points[0, 2] - points[0, 0]))
-        mp2 = np.mean(points[:, 0], points[:, 1])
-        b2 = mp2[1, 0] - m2 * mp2[0, 0]
-
-        # find intersection --> this is the center of the circle
-        xIntersect = (b2 - b1) / (m1 - m2)
-        yIntersect = xIntersect * m1 + b1
-
-        return (xIntersect, yIntersect)
-
-    # def getCentripetal(self, path):
-
-
-test = Throttle(10, 10, 10, 10, 10, 10)
-
-points = np.array([(-1, 0, 1), (0, 1, 0)])
-
-print(test.getCircle(points))
+print(Circle.getCentripetal(points, velocity))
